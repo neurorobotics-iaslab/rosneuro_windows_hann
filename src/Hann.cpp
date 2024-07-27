b30d736820f1d35a1a169fbd125d3fdacc344adf
mirovix <miroljubmihailovic98@gmail.com>
2024-07-27 16:13:04 +0200

    feat: creating dependency on  window

--- src/Hann.cpp
+++ src/Hann.cpp
@@ -1,7 +1,7 @@
 #include "rosneuro_windows_hann/Hann.hpp"
 #include "pluginlib/class_list_macros.h"
 
-PLUGINLIB_EXPORT_CLASS(rosneuro::Hann<int>,    rosneuro::Filter<int>)
-PLUGINLIB_EXPORT_CLASS(rosneuro::Hann<float>,  rosneuro::Filter<float>)
-PLUGINLIB_EXPORT_CLASS(rosneuro::Hann<double>, rosneuro::Filter<double>)
+PLUGINLIB_EXPORT_CLASS(rosneuro::Hann<int>,    rosneuro::Window<int>)
+PLUGINLIB_EXPORT_CLASS(rosneuro::Hann<float>,  rosneuro::Window<float>)
+PLUGINLIB_EXPORT_CLASS(rosneuro::Hann<double>, rosneuro::Window<double>)
 
